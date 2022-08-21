b2e5595c809f2950f28bbfa7affe4402d852c887
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-08-21 21:33:12 +0430

    feat: learned structs in c

---
+++ structs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int x;
+    int y;
+} point;
+
+typedef struct {
+    char * name;
+    int modle;
+} vehicle;
+
+int main() {
+
+    // point p;
+
+    // p.x = 10;
+    // p.y = 20;
+
+    // draw(p);
+
+    vehicle myCar;
+    myCar.modle = 2020;
+    myCar.name = "BMW";
+    
+}
