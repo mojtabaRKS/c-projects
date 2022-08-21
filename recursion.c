28909a7fc0a30c59253712ae18745f2b27061884
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-08-21 23:19:19 +0430

    style: adding some fun comments

---
+++ recursion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// this silly code is adding number y in x times :)))))
+// why we just dont use * operator ? :))))
+unsigned int multiply(unsigned int x, unsigned int y) {
+    if (x == 1) {
+        return y;
+    }
+
+    else if (x > 1) {
+        return y + multiply(x-1, y);
+    } 
+    
+    return 0;
+}
+
+int main() {
+
+    printf("3 times 5 is : %d", multiply(3, 5));
+    return 0;
+
+}
