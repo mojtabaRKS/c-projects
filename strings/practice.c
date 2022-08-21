9751aea170a3226bf95c008641b484e9ea056592
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-08-21 18:40:57 +0430

    feat: add string comparison and some other stuff

--- strings/practice.c
+++ strings/practice.c
@@ -7,10 +7,19 @@ void main() {
 
     char full_name[100];
 
-    strncat(full_name, first_name, -1);
-    strncat(full_name, " ", -1);
-    strncat(full_name, last_name, -1);
+    strncat(full_name, first_name, 10);
+    strncat(full_name, " ", 10);
+    strncat(full_name, last_name, 10);
 
-    printf("full name is : %s", full_name);
+    printf("full name is : %s \n", full_name);
+    printf("full name length is : %li \n", strlen(full_name));
+
+    int is_same = strncmp(first_name, last_name, 100);
+
+    if (is_same == 0) {
+        printf("they are same !!!! \n");
+    } else {
+        printf("oh noooooo !! \n");
+    }
 }
 
