9ce2df748d9829e9e8f1caf4ebcb03e13c677c1b
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-08-22 21:38:06 +0430

    feat: excersise for linked_list

---
+++ linked_list_practice.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    int val;
+    struct node * next;
+} node_t;
+
+void print_list(node_t * head) {
+    node_t * current = head;
+
+    while (current != NULL) {
+        printf("%d\n", current->val);
+        current = current->next;
+    }
+}
+
+int pop(node_t ** head) {
+    int retval = -1;
+    node_t * next_node = NULL;
+
+    if (*head == NULL) {
+        return -1;
+    }
+
+    next_node = (*head)->next;
+    retval = (*head)->val;
+    free(*head);
+    *head = next_node;
+
+    return retval;
+}
+
+int remove_by_value(node_t ** head, int val) {
+    /* TODO: fill in your code here */
+    int retval = -1;
+    node_t * tmp_node;
+    node_t * current = *head;
+
+    if (current->val == val) {
+        free(*head);
+        return -1;
+    }
+
+    while(current->next->next != NULL) {
+        if (current->next->val == val ) {
+            tmp_node->next = current->next->next;
+            retval = tmp_node->val;
+            current->next = tmp_node;
+            free(current);
+            return retval;
+        }
+
+        current = current->next;
+    }
+
+    return -1;
+}
+
+int main() {
+
+    node_t * test_list = (node_t *) malloc(sizeof(node_t));
+    test_list->val = 1;
+    test_list->next = (node_t *) malloc(sizeof(node_t));
+    test_list->next->val = 2;
+    test_list->next->next = (node_t *) malloc(sizeof(node_t));
+    test_list->next->next->val = 3;
+    test_list->next->next->next = (node_t *) malloc(sizeof(node_t));
+    test_list->next->next->next->val = 4;
+    test_list->next->next->next->next = NULL;
+
+    remove_by_value(&test_list, 3);
+
+    print_list(test_list);
+}
